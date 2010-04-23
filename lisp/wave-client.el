91103245a366fbb02b67e8dd0ee9fa4ea4c072de
Christian Ohler <ohler@google.com>
2010-04-24 00:18:37 +1000

    Implemented wave list over websocket.  Added basic data structures for ops.

---
+++ lisp/wave-client.el
@@ -0,0 +1,122 @@
+;;; wave-client.el --- Methods to communicate with the Wave server
+
+;; Copyright (c) 2009 Andrew Hyatt
+;;
+;; Author: Andrew Hyatt <ahyatt at gmail dot com>
+;; Maintainer: Andrew Hyatt <ahyatt at gmail dot com>
+;;
+;; Licensed to the Apache Software Foundation (ASF) under one
+;; or more contributor license agreements. See the NOTICE file
+;; distributed with this work for additional information
+;; regarding copyright ownership. The ASF licenses this file
+;; to you under the Apache License, Version 2.0 (the
+;; "License"); you may not use this file except in compliance
+;; with the License. You may obtain a copy of the License at
+;;
+;;   http://www.apache.org/licenses/LICENSE-2.0
+;;
+;; Unless required by applicable law or agreed to in writing,
+;; software distributed under the License is distributed on an
+;; "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+;; KIND, either express or implied. See the License for the
+;; specific language governing permissions and limitations
+;; under the License.
+
+
+;;; Commentary:
+
+;; This code represents a replaceable backend layer for the emacs wave
+;; mode.  It is responsible for communicating with the Wave server.
+;;
+;; This backend uses a direct connection to https://wave.google.com.
+;;
+;; WARNING: This code utilizes the unpublished client/server protocol
+;; that wave.google.com's javascript uses to communicate with the wave
+;; servers. This is not a stable protocol supported by Google. This
+;; protocol is the Google Wave team's internal protocol and is under
+;; active development, so it may change at any time without regards
+;; for this or any other client.
+
+(eval-and-compile
+  (require 'cl))
+
+(defgroup wave-client nil
+  "Wave client for emacs.")
+
+;;;###autoload
+(defcustom wave-client-user ""
+  "Name of the Wave user to connect as.  Do NOT include the
+domain for hosted accounts such as wavesandbox."
+  :type 'string 
+  :group 'wave-client)
+
+;;;###autoload
+(defcustom wave-client-domain nil
+  "Domain of the Wave server (such as `wavesandbox.com'), or nil
+for the default domain."
+  :type  '(choice (const :tag "Query when needed" nil)
+                  (string  :tag "Domain"))
+  :group 'wave-client)
+
+(defvar wave-client-temp-output
+  ""
+  "Temporary output to the REPL buffer.")
+
+(defvar wave-debug
+  nil
+  "Boolean indicating whether debug output should be sent to
+wave-client-debug-buffer")
+
+(defconst wave-debug-buffer
+  "*Wave Client Debug Buffer*"
+  "Name of the buffer wear debug output is sent to")
+
+(defun wave-client-domain ()
+  (or wave-client-domain "googlewave.com"))
+
+(defun wave-client-email-address ()
+  "Return the email address of the user."
+  (if (string-match "@" wave-client-user)
+      wave-client-user
+    (concat wave-client-user "@" (wave-client-domain))))
+
+(defun wave-debug (str &rest args)
+  "Send STR to the `wave-debug-buffer', with newline, with format
+args ARGS."
+  (when wave-debug
+    (save-excursion
+      (set-buffer
+       (get-buffer-create wave-debug-buffer))
+      (goto-char (point-max))
+      (insert (apply 'format (append (list str) args)))
+      (insert "\n"))))
+
+(defvar wave-client-connection-method 'browser-channel
+  "A symbol, either browser-channel or websocket.")
+
+
+;; Functions for the Wave mode to use:
+
+(defun wave-client-send-delta (delta)
+  "Send DELTA to the wave server."
+  (ecase wave-client-connection-method
+    (browser-channel (wave-bc-send-delta delta))
+    (websocket (wave-ws-send-delta delta))))
+
+(defun wave-get-inbox ()
+  "List all waves in the inbox. For the exact format, see
+http://code.google.com/p/wave-client-for-emacs/wiki/WaveClientDataSpec"
+  (ecase wave-client-connection-method
+    (browser-channel (wave-bc-get-inbox))
+    (websocket (wave-ws-get-inbox))))
+
+(defun wave-get-wave (wave-id)
+  "Get a wave given a WAVE-ID.  For the exact format, see
+http://code.google.com/p/wave-client-for-emacs/wiki/WaveClientDataSpec"
+  (ecase wave-client-connection-method
+    (browser-channel (wave-bc-get-wave wave-id))
+    (websocket (wave-ws-get-wave wave-id))))
+
+(provide 'wave-client)
+
+;;; wave-client.el ends here
---
+++ lisp/wave-list.el
@@ -0,0 +1,162 @@
+;;; wave-list.el --- Defines wave-list-mode, for managing waves.
+;; Copyright (c) 2009 Andrew Hyatt
+;;
+;; Author: Andrew Hyatt <ahyatt at gmail dot com>
+;; Maintainer: Andrew Hyatt <ahyatt at gmail dot com>
+;;
+;; Licensed to the Apache Software Foundation (ASF) under one
+;; or more contributor license agreements. See the NOTICE file
+;; distributed with this work for additional information
+;; regarding copyright ownership. The ASF licenses this file
+;; to you under the Apache License, Version 2.0 (the
+;; "License"); you may not use this file except in compliance
+;; with the License. You may obtain a copy of the License at
+;;
+;;   http://www.apache.org/licenses/LICENSE-2.0
+;;
+;; Unless required by applicable law or agreed to in writing,
+;; software distributed under the License is distributed on an
+;; "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+;; KIND, either express or implied. See the License for the
+;; specific language governing permissions and limitations
+;; under the License.
+
+
+;;; Commentary:
+;; This file contains code related to displaying, opening, and
+;; manipulating waves.
+;;
+;; This file right now is a work in progress, and can so far only
+;; display a list of waves.  We also need to open them, archive them,
+;; mute them, and be able to get different lists (with searches).
+
+(require 'wave-client)
+(require 'wave-display)
+
+;;; Code:
+(defface wave-list-author
+  '((((class color)) (:foreground "Green"))
+    (t (:italic t)))
+  "Face used for Wave authors."
+  :group 'wave-list-faces)
+
+(defface wave-list-unread-summary
+  '((t (:bold t)))
+  "Face used for unread Wave summaries"
+  :group 'wave-list-faces)
+
+(defface wave-list-read-summary
+  '(())
+  "Face used for read Wave summaries"
+  :group 'wave-list-faces)
+
+(defvar wave-list-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map "n" 'next-line)
+    (define-key map "p" 'previous-line)
+    (define-key map "\r" 'wave-list-open)
+    (define-key map "g" 'wave-list-refresh)
+    map)
+  "Keybindings for wave-list mode.")
+
+(defvar wave-list-waves ()
+  "The data rendered in the list of waves.")
+(make-variable-buffer-local 'wave-list-waves)
+
+(defconst wave-list-buffer-name "*Wave List*")
+
+(defun wave-list-username-only (full-username)
+  "Return the username portion of an email address FULL_USERNAME.
+
+If no @ symbol is found, return FULL-USERNAME unmodified."
+  (substring full-username 0
+             (or (string-match "@" full-username)
+                 (length full-username))))
+
+(defun wave-list-str-truncate (s max-width)
+  "Truncate string S to MAX-WIDTH chars."
+  (substring s 0 (min (length s) max-width)))
+
+(defun wave-list-open ()
+  "Open the wave at point in the list."
+  (interactive)
+  (switch-to-buffer
+   (wave-display (get-text-property (point) 'summary)
+                 (get-text-property (point) 'wave-id))))
+
+(defun wave-list-render-wave-list (wave-list)
+  "Render WAVE-LIST, a list of wave summary alists to a buffer.
+
+Every wave takes up one line."
+  (setq wave-list-waves wave-list)
+  (erase-buffer)
+  (dolist (summary-alist wave-list)
+    (let ((num-unread (plist-get summary-alist :unread)))
+      (insert
+       (let* ((unread-length 5)
+              (author-length 15)
+              (digest-length
+               (- (window-width) author-length unread-length 2)))
+         (format
+          (concat "%-" (int-to-string unread-length)
+                  "s %-" (int-to-string digest-length)
+                  "s %-" (int-to-string author-length)
+                  "s\n")
+          (if (> num-unread 0) num-unread "")
+          (wave-list-str-truncate (plist-get summary-alist :digest)
+                                  digest-length)
+          (wave-list-str-truncate
+           (replace-regexp-in-string (concat "@"
+                                             (or wave-client-domain "googlewave.com"))
+                                     ""
+                                     (plist-get summary-alist :creator))
+           author-length))))
+      (let ((bol (save-excursion (beginning-of-line 0) (point)))
+            (pre-author (save-excursion (re-search-backward " \\w+")(point)))
+            (eol (save-excursion (end-of-line 0) (point))))
+        (add-text-properties bol pre-author `(face
+                                              ,(if (> num-unread 0)
+                                                   'wave-list-unread-summary
+                                                 'wave-list-read-summary)))
+        (add-text-properties (+ 1 pre-author) eol '(face wave-list-author))
+        (add-text-properties bol eol (list 'summary (plist-get summary-alist :digest)))
+        (add-text-properties bol eol (list 'wave-id (plist-get summary-alist :id))))))
+  (goto-char (point-max))
+  (unless (eql (point) (point-min))
+    (backward-char)
+    (kill-line))
+  (goto-char (point-min)))
+
+(defun wave-list-refresh ()
+  (interactive)
+  (assert (eql major-mode 'wave-list-mode))
+  (let ((inhibit-read-only t))
+    (wave-list-render-wave-list (wave-get-inbox))))
+
+(define-derived-mode wave-list-mode nil "Wave List"
+  "Mode for displaying a list of waves.
+
+Each line in the mode represents a Wave that can be opened.
+The wave client must be connected here.
+\\{wave-list-mode-map}"
+  :group 'wave-list
+  (use-local-map wave-list-mode-map)
+  (buffer-disable-undo)
+  (setq buffer-read-only t
+	show-trailing-whitespace nil
+        truncate-lines t
+        selective-display t
+        selective-display-ellipses t)
+  (hl-line-mode))
+
+(defun wave-list ()
+  (interactive)
+  (set-buffer (get-buffer-create wave-list-buffer-name))
+  (wave-list-mode)
+  (set-window-buffer (get-buffer-window (current-buffer))
+                     wave-list-buffer-name)
+  (wave-list-refresh))
+
+(provide 'wave-list)
+
+;;; wave-list.el ends here
